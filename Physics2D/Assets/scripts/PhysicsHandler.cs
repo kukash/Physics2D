a028e7743227a9309db789704976bf1587e2ae3d
Lukas <469260@student.saxion.nl>
2020-04-03 11:27:45 +0200

    Attempt at circle line poi

--- Physics2D/Assets/scripts/PhysicsHandler.cs
+++ Physics2D/Assets/scripts/PhysicsHandler.cs
@@ -10,8 +10,10 @@ public class PhysicsHandler : MonoBehaviour
     private List<SpherePhysicsComponent> _sphereColliders;
     private List<LineCollider> _lineColliders;
     private List<GameObject> _PhysicsGameObjects;
+    private Dictionary<GameObject, PhysicsComponent> _gameObjectPhysicsComponentMap;
 
     private List<GameObject> _CollidedObjects;
+    private Dictionary<GameObject, CollisionInfo> _CollidedObjectsMap;
     void Start()
     {
         LoadPhysicsObjects();
@@ -24,7 +26,8 @@ public class PhysicsHandler : MonoBehaviour
         _physicsComponents = new List<PhysicsComponent>();
         _sphereColliders = new List<SpherePhysicsComponent>();
         _lineColliders = new List<LineCollider>();
-
+        _gameObjectPhysicsComponentMap = new Dictionary<GameObject, PhysicsComponent>();
+        _CollidedObjectsMap = new Dictionary<GameObject, CollisionInfo>();
         //get all tagged gameObjects
         GameObject[] newObjects = GameObject.FindGameObjectsWithTag("PhysicsObject");
         _PhysicsGameObjects = newObjects.ToList();
@@ -37,6 +40,7 @@ public class PhysicsHandler : MonoBehaviour
             {
                 PhysicsComponent newComponent = otherGameObject.GetComponent<PhysicsComponent>();
                 _physicsComponents.Add(newComponent);
+                _gameObjectPhysicsComponentMap.Add(otherGameObject, newComponent);
                 switch (newComponent)
                 {
                     case SpherePhysicsComponent c:
@@ -60,6 +64,16 @@ public class PhysicsHandler : MonoBehaviour
         StartUpdate();
         DetectCollision();
         ResolveCollision();
+
+        UpdateObjects();
+    }
+
+    private void UpdateObjects()
+    {
+        foreach (PhysicsComponent component in _physicsComponents)
+        {
+            if (!component.IsStatic()) component.UpdateComponent();
+        }
     }
 
     private void StartUpdate()
@@ -67,7 +81,7 @@ public class PhysicsHandler : MonoBehaviour
         foreach (PhysicsComponent component in _physicsComponents)
         {
             //isStatic always returns false if it is not overwritten
-            if (!component.IsStatic()) component.UpdateComponent();
+            if (!component.IsStatic()) component.Step();
         }
     }
 
@@ -78,6 +92,7 @@ public class PhysicsHandler : MonoBehaviour
     private void DetectCollision()
     {
         _CollidedObjects.Clear();
+        _CollidedObjectsMap.Clear();
         SphereSphereCollision();
         SphereLineCollision();
     }
@@ -89,10 +104,13 @@ public class PhysicsHandler : MonoBehaviour
             if (_CollidedObjects.Contains(physicsGameObject))
             {
                 physicsGameObject.GetComponent<SpriteRenderer>().color = Color.red;
+                _gameObjectPhysicsComponentMap[physicsGameObject].Info.Speed = 0;
+                physicsGameObject.GetComponent<PhysicsInfo>().NewPosition =
+                    _CollidedObjectsMap[physicsGameObject].PointOfImpact;
             }
             else
             {
-                if(!physicsGameObject.GetComponent<PhysicsComponent>().IsStatic())
+                if (!physicsGameObject.GetComponent<PhysicsComponent>().IsStatic())
                     physicsGameObject.GetComponent<SpriteRenderer>().color = Color.green;
 
             }
@@ -109,6 +127,9 @@ public class PhysicsHandler : MonoBehaviour
                 if (info.hit)
                 {
                     _CollidedObjects.Add(sphere.gameObject);
+                    Vector2 poi = SphereLinePOI(sphere, line);
+                    info.PointOfImpact = poi;
+                    _CollidedObjectsMap.Add(sphere.gameObject, info);
                 }
             }
         }
@@ -133,16 +154,61 @@ public class PhysicsHandler : MonoBehaviour
         }
     }
 
-    private CollisionInfo Sphere_Line_Intersection(SpherePhysicsComponent sphere, LineCollider line)
+    private Vector2 SphereLinePOI(SpherePhysicsComponent sphere, LineCollider line)
     {
-        CollisionInfo info = new CollisionInfo();
-        Vector2 Delta = sphere.GetPosition() -line.GetLineStart() ;
-      
+
+        PhysicsInfo sphereInfo = sphere.GetComponent<PhysicsInfo>();
+        Vector2 pointOfImpact = Vector2.zero;
+
+        if (sphereInfo.Speed > 0)
+        {
+            //Debug.Log("Old position" + sphereInfo.OldPosition);
+            //Debug.Log("old distance :" + SphereLineDistance(sphereInfo.OldPosition, line));
+            //Debug.Log("New position" + sphereInfo.NewPosition);
+            //Debug.Log("new distance :" + SphereLineDistance(sphereInfo.NewPosition, line));
+            float oldDistance = SphereLineDistance(sphereInfo.OldPosition, line);
+            float newDistance = SphereLineDistance(sphereInfo.NewPosition, line);
+            float a = oldDistance - sphere.GetRadius();
+            float b = oldDistance + newDistance;
+            //   Vector2 delta = sphereInfo.NewPosition - sphereInfo.OldPosition;
+
+            // float b = Vector2DFunctions.Length(delta);
+            float t = a / b;
+
+            Vector2 offset = sphereInfo.Direction.normalized * t * sphereInfo.Velocity;
+            //a<b
+            //Debug.Log("Delta " + delta);
+            //Debug.Log("a: " + a);
+            //Debug.Log("b: " + b);
+            //Debug.Log("t: " + t);
+            //Debug.Log("speed " + sphereInfo.Speed);
+            //Debug.Log("Dir " + sphereInfo.Direction);
+            //Debug.Log("offset " + offset);
+            pointOfImpact = sphereInfo.OldPosition + offset;
+            // Debug.Log("poi: " + POI);
+
+        }
+
+        return pointOfImpact;
+    }
+
+    private float SphereLineDistance(Vector2 position, LineCollider line)
+    {
+        float distance = 0;
+        Vector2 Delta = position - line.GetLineStart();
+
         float dot = Vector2.Dot(line.GetLineDir(), Delta);
         Vector2 newPos = line.GetLineStart() + line.GetLineDir().normalized * dot;
 
-        float distance = Vector2.Distance(newPos, sphere.GetPosition());
-        if (distance < sphere.GetRadius()) info.hit = true;
+        distance = Vector2.Distance(newPos, position);
+        return distance;
+
+    }
+    private CollisionInfo Sphere_Line_Intersection(SpherePhysicsComponent sphere, LineCollider line)
+    {
+        CollisionInfo info = new CollisionInfo();
+
+        if (SphereLineDistance(sphere.GetPosition(), line) < sphere.GetRadius()) info.hit = true;
         return info;
     }
     private CollisionInfo Sphere_SphereIntersection(SpherePhysicsComponent sphereA, SpherePhysicsComponent sphereB)
@@ -164,7 +230,7 @@ public class PhysicsHandler : MonoBehaviour
 public class CollisionInfo
 {
     public bool hit;
-
+    public Vector2 PointOfImpact = Vector2.zero;
     public CollisionInfo()
     {
         hit = false;
---
+++ Physics2D/Assets/scripts/PhysicsInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsInfo : MonoBehaviour
+{
+    public float Speed;
+    public Vector2 Direction = Vector2.zero;
+    public Vector2 Velocity = Vector2.zero;
+    public Vector2 OldPosition = Vector2.zero;
+    public Vector2 NewPosition = Vector2.zero;
+}
